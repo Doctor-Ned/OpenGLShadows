acb6080c1bf4f9c3a0a6547ff9859760cbdcd10a
Doctor Ned <1933864+Doctor-Ned@users.noreply.github.com>
2021-01-24 15:53:53 +0100

    Added ImGui FPS

--- OpenGLShadowsExec/OpenGLShadowsExec.cpp
+++ OpenGLShadowsExec/OpenGLShadowsExec.cpp
@@ -99,6 +99,7 @@ int main()
     auto guiProc = [&]()
     {
         ImGui::Begin("Settings");
+        ImGui::Text("FPS: %f", ImGui::GetIO().Framerate);
         ImGui::Checkbox("Show settings", &showingSettings);
         if (showingSettings)
         {
@@ -146,7 +147,7 @@ int main()
         if (static_cast<unsigned int>(time) > secondCounter)
         {
             secondCounter = static_cast<unsigned int>(time);
-            SHADOW_INFO("{} FPS", appWindow.getFps());
+            SHADOW_INFO("{} FPS", ImGui::GetIO().Framerate);
         }
     }
     return 0;
